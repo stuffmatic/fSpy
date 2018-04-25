9f2dd817dc06801628114b42ec180f0dd807989f
per <per@stuffmatic.com>
2018-04-25 17:25:17 +0200

    slight default state handling cleanup

--- react-app/src/reducers/control-points.tsx
+++ react-app/src/reducers/control-points.tsx
@@ -1,22 +1,32 @@
 import { AppAction, SET_PRINCIPAL_POINT } from '../actions';
-import { ControlPointsState1VP, ControlPointsState2VP } from '../types/store-state';
+import { ControlPointsState1VP, ControlPointsState2VP, ControlPointsStateBase } from '../types/store-state';
+
+const defaultControlPointsStateBase:ControlPointsStateBase = {
+  principalPoint: {
+    x: 0.4, y: 0.2
+  },
+  origin: {
+    x: 0.5, y: 0.5
+  }
+}
+
+const defaultControlPointsState1VP:ControlPointsState1VP = {
+  ...defaultControlPointsStateBase,
+  horizonStart: {
+    x: 0.2, y: 0.5
+  },
+  horizonEnd: {
+    x: 0.8, y: 0.5
+  }
+}
+
+const defaultControlPointsState2VP:ControlPointsState2VP = {
+  ...defaultControlPointsStateBase
+}
 
 export function controlPointsState1VP(state: ControlPointsState1VP, action: AppAction): ControlPointsState1VP {
   if (state === undefined) {
-    return {
-      principalPoint: {
-        x: 0.4, y: 0.2
-      },
-      origin: {
-        x: 0.5, y: 0.5
-      },
-      horizonStart: {
-        x: 0.2, y: 0.5
-      },
-      horizonEnd: {
-        x: 0.8, y: 0.5
-      }
-    }
+    return defaultControlPointsState1VP
   }
 
   switch (action.type) {
@@ -31,14 +41,7 @@ export function controlPointsState1VP(state: ControlPointsState1VP, action: AppA
 
 export function controlPointsState2VP(state: ControlPointsState2VP, action: AppAction): ControlPointsState2VP {
   if (state === undefined) {
-    return {
-      principalPoint: {
-        x: 0.4, y: 0.2
-      },
-      origin: {
-        x: 0.5, y: 0.5
-      }
-    }
+    return defaultControlPointsState2VP
   }
 
   switch (action.type) {
