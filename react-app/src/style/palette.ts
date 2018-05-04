49d9458f2ffacb0fad8579ba3960a3175da22690
per <per@stuffmatic.com>
2018-05-04 09:32:18 +0200

    add palette enum for color constants

---
+++ react-app/src/style/palette.ts
@@ -0,0 +1,11 @@
+export enum Palette {
+  Red = "#EC5D57",
+  Green = "#70BF41",
+  Blue = "#51A7F9",
+  Orange = "#F39019",
+  Yellow = "#F3CA05",
+  White = "#FFFFFF",
+  LightGray = "#F5F5F5",
+  Gray = "#D1D3D5",
+  Black = "#303030"
+}
