dd6626316864add65f56431ce26c6f8e39ccc73d
per <per@stuffmatic.com>
2018-05-07 22:26:58 +0200

    proof of concept modal export dialog skeleton

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ImageContainer from './containers/image-container';
 import ResultContainer from './containers/result-container';
 import SettingsContainer from './containers/settings-container';
+import ExportDialog from './components/export-dialog/export-dialog';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ import './App.css';
 function App() {
   return (
     <div id="app-container">
+      <ExportDialog />
       <SettingsContainer />
       <ImageContainer  />
       <ResultContainer />
---
+++ react-app/src/components/export-dialog/export-dialog.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Button from './../common/button'
+
+
+export default function ExportDialog() {
+  let modalColumnStyle:React.CSSProperties = {
+    flexBasis: "50%",
+    padding: "20px",
+    overflow: "auto"
+  }
+  return (
+    <div id="modal-container" style={{
+      position: "fixed",
+      zIndex: 1,
+      left: 0,
+      top: 0,
+      width: "100%",
+      height: "100%",
+      overflow: "hidden",
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    }
+    }>
+      <div id="modal-top-buttons" style={{display: "flex"}}>
+        <Button title="Herro" onClick={ () => {}} />
+        <Button title="Herro" onClick={ () => {}} />
+        <Button title="Herro" onClick={ () => {}} />
+      </div>
+      <div id="modal-content" style={{
+         backgroundColor: "#fefefe",
+         width: "700px",
+         height: "500px",
+         display: "flex",
+         flexDirection: "column"
+      }}>
+        <div id="modal-columns" style={{
+          height: "450px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center"
+        }}>
+          <div style={modalColumnStyle}>
+            Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+            Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+            efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+            av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+            vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+            avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+            av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+            vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+
+          </div>
+          <div style={modalColumnStyle}>
+
+            omg code hee
+
+          </div>
+        </div>
+        <div id="modal-bottom-button" style= {{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexGrow: 1
+        }}>
+          <Button title="Close" onClick={ () => {}} />
+        </div>
+      </div>
+    </div>
+  )
+}
