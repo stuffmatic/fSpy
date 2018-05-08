5e13af14c0e2955efd50be7e51cefd431d91bb0c
per <per@stuffmatic.com>
2018-05-08 07:56:50 +0200

    more work on export panel skeleton

--- react-app/src/components/common/button.tsx
+++ react-app/src/components/common/button.tsx
@@ -3,11 +3,26 @@ import * as React from 'react';
 interface ButtonProps {
   title: string
   onClick(): void
+  fillWidth?: boolean
 }
 
 export default function Button(props: ButtonProps) {
+
+  let style = {
+    minWidth: "90px",
+    height: "25px",
+    backgroundColor: "#e0e0e0",
+    outline: "none",
+    border: "none",
+    boxShadow: "none"
+  }
+
+  if (props.fillWidth === true) {
+    (style as any).width = "100%";
+  }
+
   return (
-    <button className={"button"} onClick={() => props.onClick()}>
+    <button style={style} onClick={() => props.onClick()}>
       {props.title}
     </button>
   )
--- react-app/src/components/export-dialog/export-dialog.tsx
+++ react-app/src/components/export-dialog/export-dialog.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 import Button from './../common/button'
+import { Palette } from '../../style/palette';
 
 
 export default function ExportDialog() {
-  let modalColumnStyle:React.CSSProperties = {
+  let modalColumnStyle: React.CSSProperties = {
     flexBasis: "50%",
-    padding: "20px",
-    overflow: "auto"
+    padding: "25px",
+
   }
+
+  let modalColumnContentStyle: React.CSSProperties = {
+    overflow: "auto",
+    height: "100%"
+  }
+
+  let modalColumnCodeContentStyle: React.CSSProperties = {
+    overflow: "auto",
+    height: "100%",
+    backgroundColor: Palette.black,
+    color: Palette.white,
+    fontFamily: "Roboto Mono"
+  }
+
   return (
     <div id="modal-container" style={{
       position: "fixed",
@@ -24,17 +39,17 @@ export default function ExportDialog() {
       justifyContent: "center"
     }
     }>
-      <div id="modal-top-buttons" style={{display: "flex"}}>
-        <Button title="Herro" onClick={ () => {}} />
-        <Button title="Herro" onClick={ () => {}} />
-        <Button title="Herro" onClick={ () => {}} />
+      <div id="modal-top-buttons" style={{ display: "flex" }}>
+        <Button title="Herro" onClick={() => { }} />
+        <Button title="Herro" onClick={() => { }} />
+        <Button title="Herro" onClick={() => { }} />
       </div>
       <div id="modal-content" style={{
-         backgroundColor: "#fefefe",
-         width: "700px",
-         height: "500px",
-         display: "flex",
-         flexDirection: "column"
+        backgroundColor: "#fefefe",
+        width: "700px",
+        height: "500px",
+        display: "flex",
+        flexDirection: "column"
       }}>
         <div id="modal-columns" style={{
           height: "450px",
@@ -43,29 +58,67 @@ export default function ExportDialog() {
           justifyContent: "center"
         }}>
           <div style={modalColumnStyle}>
-            Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
-            Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
-            efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
-            av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
-            vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
-            avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
-            av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
-            vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+            <div style={modalColumnContentStyle} >
+              Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+              Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+              efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+              Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+              efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+              Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+              efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
 
+            </div>
           </div>
+
           <div style={modalColumnStyle}>
-
-            omg code hee
-
+            <div style={modalColumnCodeContentStyle}>
+            <div style={ {padding: "10px"} }>
+            lyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+              Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+              efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem
+              Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att
+              efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning
+              avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland
+              av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska). av olyckshändelse, ibland med flit (mer eller mindre humoristiska).
+              vara läsbar text. Många publiseringprogram
+              </div>
+            </div>
           </div>
         </div>
-        <div id="modal-bottom-button" style= {{
+        <div id="modal-bottom-button" style={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           flexGrow: 1
         }}>
-          <Button title="Close" onClick={ () => {}} />
+          <Button title="Close" onClick={() => { }} />
         </div>
       </div>
     </div>
