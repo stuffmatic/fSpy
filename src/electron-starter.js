850c8d99e509f57ef5cfaef139912584fc6b2a59
per <per@stuffmatic.com>
2018-05-18 21:07:13 +0200

    avoid electron window flickering by shoing the window once the content has loaded

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, show: false} )
+  mainWindow.on('ready-to-show', function() {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
   mainWindow.setBackgroundColor("#222222")
   // and load the index.html of the app.
   /*mainWindow.loadURL(url.format({
