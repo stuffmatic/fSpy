2469384e627bef2fb5ec2d76e09b7b96671eecf9
per <per@stuffmatic.com>
2018-05-18 15:23:01 +0200

    set electron window background color

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -14,7 +14,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  mainWindow.setBackgroundColor("#222222")
   // and load the index.html of the app.
   /*mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
