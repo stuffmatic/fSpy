6cb7cf553e70fa4d0c995193f00539bd9d86aba0
per <per@stuffmatic.com>
2018-06-03 22:35:36 +0200

    import missing styles

--- src/gui/index.tsx
+++ src/gui/index.tsx
@@ -4,6 +4,11 @@ import App from './App'
 import store from './store/store'
 import { Provider } from 'react-redux'
 
+import './App.css'
+import './index.css'
+import 'react-select/dist/react-select.css'
+import 'highlight.js/styles/atom-one-dark.css'
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
