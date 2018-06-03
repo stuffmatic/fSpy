60a94b25340d89320cbbde88bfef6aaa8a44fe02
per <per@stuffmatic.com>
2018-06-03 12:06:31 +0200

    port old store setup code

---
+++ src/gui/store/recalculation-trigger-middleware.ts
@@ -0,0 +1,15 @@
+import { Middleware } from 'redux'
+import { StoreState } from '../types/store-state'
+import { actionTypesTriggeringRecalculation, recalculateCalibrationResult } from '../actions'
+import store from './store'
+
+// Middleware for requesting calibration result recalculation for
+// action types matching a set of given types
+export const recalculationTriggerMiddleware: Middleware<{}, StoreState> = _ => next => action => {
+
+  if (actionTypesTriggeringRecalculation.indexOf(action.type) >= 0) {
+    store.dispatch<any>(recalculateCalibrationResult())
+  }
+
+  return next(action)
+}
