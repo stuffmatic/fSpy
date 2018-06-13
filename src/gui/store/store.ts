377daa511a7b6092449c1de895ca36f767694a71
per <per@stuffmatic.com>
2018-06-13 13:42:21 +0200

    store fixes

--- src/gui/store/store.ts
+++ src/gui/store/store.ts
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, AnyAction } from 'redux'
 import rootReducer from '../reducers/root'
 import { Store } from 'react-redux'
 import { StoreState } from '../types/store-state'
-import { AppAction } from '../actions'
 import thunk from 'redux-thunk'
 import { recalculationTriggerMiddleware } from './recalculation-trigger-middleware'
 
-const store: Store<any> = createStore<StoreState, AppAction, {}, {}>(
+const store: Store<any> = createStore<StoreState, AnyAction, {}, {}>(
   rootReducer,
-  // TODO: only in dev builds
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(recalculationTriggerMiddleware, thunk)
 )
 
