e4f29de277eeea4d77c85cff6fe4dcdccc55087c
per <per@stuffmatic.com>
2018-11-13 21:24:36 +0100

    add license header to source files

--- src/gui/types/store-state.ts
+++ src/gui/types/store-state.ts
@@ -1,3 +1,21 @@
+/**
+ * fSpy
+ * Copyright (C) 2018 - Per Gantelius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { GlobalSettings } from './global-settings'
 import { ControlPointsState2VP, ControlPointsState1VP, ControlPointsStateBase } from './control-points-state'
 import { CalibrationSettings2VP, CalibrationSettings1VP, CalibrationSettingsBase } from './calibration-settings'
