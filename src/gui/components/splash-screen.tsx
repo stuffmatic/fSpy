b8e0589b6aee8bcea81fc0ad3d21bf073d4a1c42
per <per@stuffmatic.com>
2018-11-14 07:47:24 +0100

    fix splash screen layout issues

--- src/gui/components/splash-screen.tsx
+++ src/gui/components/splash-screen.tsx
@@ -27,10 +27,10 @@ interface SplashScreenProps {
 export default function SplashScreen(props: SplashScreenProps) {
   return (
     <div style={{ backgroundColor: Palette.imagePanelBackgroundColor, width: '100vw', height: '100vh', position: 'absolute' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', marginTop: '50px', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'center' }}>
         <div style={{ textAlign: 'center', alignSelf: 'center' }}>
           <img
-            style={{ width: '100px', height: '100px', paddingBottom: '20px' }}
+            style={{ width: '100px', marginTop: '100px', marginBottom: '30px', height: '100px' }}
             src={resourceURL('icon.svg')}
           />
           <div style={{ color: 'white', opacity: 0.3 }}>Drop an image or project here</div>
