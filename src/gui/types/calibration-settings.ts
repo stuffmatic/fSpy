2a4707a86a34d9503f41b14876711ec6cf5a2161
Per <per@stuffmatic.com>
2020-04-14 07:39:20 +0200

    update license header copyright info

--- src/gui/types/calibration-settings.ts
+++ src/gui/types/calibration-settings.ts
@@ -2,7 +2,7 @@ import { CameraPreset } from '../solver/camera-presets'
 
 /**
  * fSpy
- * Copyright (C) 2018 - Per Gantelius
+ * Copyright (c) 2020 - Per Gantelius
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
